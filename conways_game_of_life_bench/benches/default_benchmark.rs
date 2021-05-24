fa06b7c6c05ccca8a609e8881142ff8b1d98996e
Bobby <bobby@00384fe957d394corl-2.local>
2021-05-24 20:42:42 +0100

    Added LinearGrid to benchmark.

--- conways_game_of_life_bench/benches/default_benchmark.rs
+++ conways_game_of_life_bench/benches/default_benchmark.rs
@@ -1,11 +1,8 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{BenchmarkGroup, criterion_group, criterion_main, Criterion, measurement::WallTime};
 use conways_game_of_life_impl::{Grid, Grid2d, LinearGrid};
 use std::time::Duration;
 
-fn criterion_benchmark(c: &mut Criterion) {
-    //let mut game = LinearGrid::empty((80, 21));
-    let mut game = Grid2d::empty((80, 21));
-
+fn init_benchmark<G: Grid>(name: &str, mut game: G, group: &mut BenchmarkGroup<'_, WallTime>) {
     game.block((1, 1)).unwrap();
     game.bee_hive((5, 1)).unwrap();
     game.loaf((11, 1)).unwrap();
@@ -17,13 +14,21 @@ fn criterion_benchmark(c: &mut Criterion) {
     game.pulsar((45, 1)).unwrap();
     game.penta_decathlon((64, 3)).unwrap();
 	
-    let mut group = c.benchmark_group("bgroup");
-    group.measurement_time(Duration::from_secs(10));
-    group.bench_function("default", |b| {
+    group.bench_function(name, |b| {
         b.iter(|| {
 	    game.update();
         })
     });
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("bgroup");
+    group.measurement_time(Duration::from_secs(10));
+
+    let size = (1000, 1000);
+    init_benchmark("linear", LinearGrid::empty(size), &mut group);
+    init_benchmark("2d", Grid2d::empty(size), &mut group);
+
     group.finish();
 }
 
